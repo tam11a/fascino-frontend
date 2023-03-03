52f1f36a8cde9453c96e43a7b505d51caf3d0153
Asatter833 <asatter833@gmail.com>
2023-03-03 08:22:48 +0600

    Branch create, list fetched and basic routing done

---
+++ src/pages/BrancheInfo/routes/index.tsx
@@ -0,0 +1,24 @@
+import NProgressSuspense from "@components/NProgressSuspense";
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { branchRoutes } from "./branchRoutes";
+
+const EmployeeRoutes: React.FC = () => {
+  return (
+    <Routes>
+      {branchRoutes?.map?.(({ path, Component }) => (
+        <Route
+          key={path}
+          path={path}
+          element={
+            <Suspense fallback={<NProgressSuspense />}>
+              <Component />
+            </Suspense>
+          }
+        />
+      ))}
+    </Routes>
+  );
+};
+
+export default EmployeeRoutes;
