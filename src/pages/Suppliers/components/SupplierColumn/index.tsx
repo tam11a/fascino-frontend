d67ea7b94e522f7b2f76fbac838c3bb8f01c24b2
Asatter833 <asatter833@gmail.com>
2023-03-16 23:47:36 +0600

    suppliers email typography fixed

--- src/pages/Suppliers/components/SupplierColumn/index.tsx
+++ src/pages/Suppliers/components/SupplierColumn/index.tsx
@@ -1,5 +1,5 @@
 // import defaultPermissions from "@/utilities/defaultPermissions";
-import { Chip, IconButton, Typography } from "@mui/material";
+import { Chip, IconButton } from "@mui/material";
 import { GridColumns } from "@mui/x-data-grid";
 import { IDataTable } from "@pages/Suppliers/types";
 // import { checkAccess } from "@tam11a/react-use-access";
@@ -8,111 +8,109 @@ import { FiEdit2 } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 
 const SupplierColumn = (): GridColumns<IDataTable> => {
-	const navigate = useNavigate();
-	return [
-		{
-			headerName: "ID",
-			headerAlign: "center",
-			field: "_id",
-			align: "center",
-			width: 200,
-			// flex: 1,
-			sortable: false,
-			hide: true,
-		},
-		{
-			headerName: "Name",
-			headerAlign: "center",
-			field: "name",
-			align: "center",
-			width: 150,
-			minWidth: 150,
-			flex: 1,
-		},
+  const navigate = useNavigate();
+  return [
+    {
+      headerName: "ID",
+      headerAlign: "center",
+      field: "_id",
+      align: "center",
+      width: 200,
+      // flex: 1,
+      sortable: false,
+      hide: true,
+    },
+    {
+      headerName: "Name",
+      headerAlign: "center",
+      field: "name",
+      align: "center",
+      width: 150,
+      minWidth: 150,
+      flex: 1,
+    },
 
-		{
-			headerName: "Phone",
-			headerAlign: "center",
-			field: "phone",
-			align: "center",
-			flex: 1,
-			width: 160,
-			minWidth: 150,
-		},
-		{
-			headerName: "Email",
-			headerAlign: "center",
-			field: "email",
-			width: 250,
-			minWidth: 250,
-			flex: 1.5,
-			align: "center",
-			renderCell: (data: any) =>
-				data.row?.email ? <Typography>{data.row?.email}</Typography> : "-",
-		},
-		{
-			headerName: "Created by",
-			field: "createdBy",
-			width: 170,
-			minWidth: 150,
-			headerAlign: "center",
-			align: "center",
-			flex: 1,
-			renderCell: (data: any) =>
-				data.row?.createdBy?.userName ? (
-					<Chip
-						label={data.row?.createdBy?.userName}
-						// sx={{
-						//   textTransform: "uppercase",
-						// }}
-					/>
-				) : (
-					"-"
-				),
-		},
-		{
-			headerName: "Updated By",
-			field: "updatedBy",
-			width: 220,
-			minWidth: 150,
-			flex: 1,
-			headerAlign: "center",
-			align: "center",
-			renderCell: (data: any) =>
-				data.row?.updatedBy?.userName ? (
-					<Chip
-						label={data.row?.updatedBy?.userName}
-						// sx={{
-						//   textTransform: "uppercase",
-						// }}
-					/>
-				) : (
-					"-"
-				),
-		},
-		{
-			headerName: "Action",
-			field: "action",
-			width: 70,
-			minWidth: 60,
-			// flex: 1,
-			headerAlign: "center",
-			align: "center",
-			renderCell: (data: any) => (
-				<>
-					<IconButton
-						sx={{ fontSize: "large" }}
-						color="primary"
-						onClick={() => navigate(`/app/supplier/${data.row?._id}`)}
-						// disabled={!checkAccess(defaultPermissions.SupplierS.FULL)}
-						disabled
-					>
-						<FiEdit2 />
-					</IconButton>
-				</>
-			),
-		},
-	];
+    {
+      headerName: "Phone",
+      headerAlign: "center",
+      field: "phone",
+      align: "center",
+      flex: 1,
+      width: 160,
+      minWidth: 150,
+    },
+    {
+      headerName: "Email",
+      headerAlign: "center",
+      field: "email",
+      width: 250,
+      minWidth: 250,
+      flex: 1.5,
+      align: "center",
+    },
+    {
+      headerName: "Created by",
+      field: "createdBy",
+      width: 170,
+      minWidth: 150,
+      headerAlign: "center",
+      align: "center",
+      flex: 1,
+      renderCell: (data: any) =>
+        data.row?.createdBy?.userName ? (
+          <Chip
+            label={data.row?.createdBy?.userName}
+            // sx={{
+            //   textTransform: "uppercase",
+            // }}
+          />
+        ) : (
+          "-"
+        ),
+    },
+    {
+      headerName: "Updated By",
+      field: "updatedBy",
+      width: 220,
+      minWidth: 150,
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: (data: any) =>
+        data.row?.updatedBy?.userName ? (
+          <Chip
+            label={data.row?.updatedBy?.userName}
+            // sx={{
+            //   textTransform: "uppercase",
+            // }}
+          />
+        ) : (
+          "-"
+        ),
+    },
+    {
+      headerName: "Action",
+      field: "action",
+      width: 70,
+      minWidth: 60,
+      // flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: (data: any) => (
+        <>
+          <IconButton
+            sx={{ fontSize: "large" }}
+            color="primary"
+            onClick={() => navigate(`/app/supplier/${data.row?._id}`)}
+            // disabled={!checkAccess(defaultPermissions.SupplierS.FULL)}
+            disabled
+          >
+            <FiEdit2 />
+          </IconButton>
+        </>
+      ),
+    },
+  ];
 };
 
 export default SupplierColumn;
