421ccc37eb12fea136c085c20a659d9e91c17917
Asatter833 <asatter833@gmail.com>
2023-03-22 20:36:44 +0600

    Shipment fetched

---
+++ src/queries/shipment/types.ts
@@ -0,0 +1,11 @@
+export type IShipment = {
+  supplier: string;
+  weight: number;
+  buyingPrice: number;
+  shipmentCost: number;
+  supplierCommision: number;
+  sellPrice: number;
+  tax: number;
+  product: string;
+  quantity: number;
+};
