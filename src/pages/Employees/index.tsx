04460f9e2e8a819034aedbd40a46b1999f932e9f
Asatter833 <asatter833@gmail.com>
2023-04-15 05:27:12 +0600

    all back buttons removed

--- src/pages/Employees/index.tsx
+++ src/pages/Employees/index.tsx
@@ -6,7 +6,6 @@ import EmployeeColumn from "./components/EmployeeColumn";
 // import { AccessMargin } from "@tam11a/react-use-access";
 // import defaultPermissions from "@/utilities/defaultPermissions";
 import { useGetEmployees } from "@/queries/employees";
-import BackButton from "@components/BackButton";
 import { FloatButton, Input } from "antd";
 import { BsSearch } from "react-icons/bs";
 import Iconify from "@components/iconify";
@@ -15,86 +14,71 @@ import Iconify from "@components/iconify";
 const DataTable = React.lazy(() => import("@/components/Datatable"));
 
 const Employees: React.FC = () => {
-	const { limit, setLimit, page, setPage, getQueryParams, search, setSearch } =
-		usePaginate();
+  const { limit, setLimit, page, setPage, getQueryParams, search, setSearch } =
+    usePaginate();
 
-	const { data, isLoading } = useGetEmployees(getQueryParams());
-	const { state: open, toggleState: onClose } = useToggle(false);
+  const { data, isLoading } = useGetEmployees(getQueryParams());
+  const { state: open, toggleState: onClose } = useToggle(false);
 
-	return (
-		<>
-			<Container
-				maxWidth={"lg"}
-				sx={{
-					maxWidth: "1500px !important",
-				}}
-			>
-				<Grid
-					container
-					rowGap={2}
-					direction="column"
-					marginTop={4}
-				>
-					<Grid className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-3">
-						<div className="flex flex-row items-center ">
-							<BackButton />
-							<Typography
-								variant="subtitle1"
-								fontWeight={700}
-							>
-								{/* {t("employee:EmployeeList")} */}
-								Employees
-							</Typography>
-						</div>
-						<Input
-							className="w-full sm:max-w-xs"
-							placeholder="Search Employees"
-							suffix={<BsSearch />}
-							value={search}
-							onChange={(e) => setSearch(e.target.value)}
-							style={{ width: "auto" }}
-							size="large"
-							allowClear
-						/>
-					</Grid>
-					<Grid item>
-						<DataTable
-							columns={EmployeeColumn()}
-							rows={data?.data?.data || []}
-							isLoading={isLoading}
-							getRowId={(r: any) => r?._id}
-							// ss pagination
-							rowCount={data?.data?.total || 0}
-							paginationMode={"server"}
-							page={page}
-							onPageChange={setPage}
-							pageSize={limit}
-							onPageSizeChange={setLimit}
-						/>
-					</Grid>
-				</Grid>
+  return (
+    <>
+      <Container
+        maxWidth={"lg"}
+        sx={{
+          maxWidth: "1500px !important",
+        }}
+      >
+        <Grid container rowGap={2} direction="column" marginTop={4}>
+          <Grid className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-3">
+            <div className="flex flex-row items-center ">
+              <Typography variant="subtitle1" fontWeight={700}>
+                {/* {t("employee:EmployeeList")} */}
+                Employees
+              </Typography>
+            </div>
+            <Input
+              className="w-full sm:max-w-xs"
+              placeholder="Search Employees"
+              suffix={<BsSearch />}
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              style={{ width: "auto" }}
+              size="large"
+              allowClear
+            />
+          </Grid>
+          <Grid item>
+            <DataTable
+              columns={EmployeeColumn()}
+              rows={data?.data?.data || []}
+              isLoading={isLoading}
+              getRowId={(r: any) => r?._id}
+              // ss pagination
+              rowCount={data?.data?.total || 0}
+              paginationMode={"server"}
+              page={page}
+              onPageChange={setPage}
+              pageSize={limit}
+              onPageSizeChange={setLimit}
+            />
+          </Grid>
+        </Grid>
 
-				<FloatButton.Group
-					shape="square"
-					className="bottom-20 sm:bottom-4"
-				>
-					<FloatButton
-						icon={<Iconify icon={"material-symbols:filter-alt-outline"} />}
-					/>
-					<FloatButton
-						icon={<Iconify icon={"material-symbols:add"} />}
-						onClick={() => onClose()}
-					/>
-				</FloatButton.Group>
+        <FloatButton.Group shape="square" className="bottom-20 sm:bottom-4">
+          <FloatButton
+            icon={<Iconify icon={"material-symbols:filter-alt-outline"} />}
+          />
+          <FloatButton
+            icon={<Iconify icon={"material-symbols:add"} />}
+            onClick={() => onClose()}
+          />
+        </FloatButton.Group>
 
-				{/* Dialog Box */}
-				<CreateEmployee
-					open={open}
-					onClose={onClose}
-				/>
-			</Container>
-		</>
-	);
+        {/* Dialog Box */}
+        <CreateEmployee open={open} onClose={onClose} />
+      </Container>
+    </>
+  );
 };
 
 export default Employees;
--- src/pages/Tailor/index.tsx
+++ src/pages/Tailor/index.tsx
@@ -1,5 +1,4 @@
 import { useGetTailor } from "@/queries/tailor";
-import BackButton from "@components/BackButton";
 import DataTable from "@components/Datatable";
 import Iconify from "@components/iconify";
 import { Container, Grid, Typography } from "@mui/material";
@@ -11,86 +10,71 @@ import CreateTailor from "./components/CreateTailor";
 import TailorColumn from "./components/TailorColumn";
 
 const Tailor: React.FC = () => {
-	const { limit, setLimit, page, setPage, getQueryParams, setSearch, search } =
-		usePaginate();
+  const { limit, setLimit, page, setPage, getQueryParams, setSearch, search } =
+    usePaginate();
 
-	const { data, isLoading } = useGetTailor(getQueryParams());
+  const { data, isLoading } = useGetTailor(getQueryParams());
 
-	const { state: open, toggleState: onClose } = useToggle(false);
+  const { state: open, toggleState: onClose } = useToggle(false);
 
-	return (
-		<>
-			<Container
-				maxWidth={"lg"}
-				sx={{
-					maxWidth: "1500px !important",
-				}}
-			>
-				<Grid
-					container
-					rowGap={2}
-					direction="column"
-					marginTop={4}
-				>
-					<Grid className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-3">
-						<div className="flex flex-row items-center ">
-							<BackButton />
-							<Typography
-								variant="subtitle1"
-								fontWeight={700}
-							>
-								{/* {t("employee:EmployeeList")} */}
-								Tailors
-							</Typography>
-						</div>
-						<Input
-							className="w-full sm:max-w-xs"
-							placeholder="Search Tailor"
-							suffix={<BsSearch />}
-							value={search}
-							onChange={(e) => setSearch(e.target.value)}
-							style={{ width: "auto" }}
-							size="large"
-							allowClear
-						/>
-					</Grid>
+  return (
+    <>
+      <Container
+        maxWidth={"lg"}
+        sx={{
+          maxWidth: "1500px !important",
+        }}
+      >
+        <Grid container rowGap={2} direction="column" marginTop={4}>
+          <Grid className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-3">
+            <div className="flex flex-row items-center ">
+              <Typography variant="subtitle1" fontWeight={700}>
+                {/* {t("employee:EmployeeList")} */}
+                Tailors
+              </Typography>
+            </div>
+            <Input
+              className="w-full sm:max-w-xs"
+              placeholder="Search Tailor"
+              suffix={<BsSearch />}
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              style={{ width: "auto" }}
+              size="large"
+              allowClear
+            />
+          </Grid>
 
-					<Grid item>
-						<DataTable
-							columns={TailorColumn()}
-							rows={data?.data?.data || []}
-							isLoading={isLoading}
-							getRowId={(r: any) => r?._id}
-							rowCount={data?.data?.total || 0}
-							paginationMode={"server"}
-							page={page}
-							onPageChange={setPage}
-							pageSize={limit}
-							onPageSizeChange={setLimit}
-						/>
-					</Grid>
-				</Grid>
+          <Grid item>
+            <DataTable
+              columns={TailorColumn()}
+              rows={data?.data?.data || []}
+              isLoading={isLoading}
+              getRowId={(r: any) => r?._id}
+              rowCount={data?.data?.total || 0}
+              paginationMode={"server"}
+              page={page}
+              onPageChange={setPage}
+              pageSize={limit}
+              onPageSizeChange={setLimit}
+            />
+          </Grid>
+        </Grid>
 
-				<FloatButton.Group
-					shape="square"
-					className="bottom-20 sm:bottom-4"
-				>
-					<FloatButton
-						icon={<Iconify icon={"material-symbols:filter-alt-outline"} />}
-					/>
-					<FloatButton
-						icon={<Iconify icon={"material-symbols:add"} />}
-						onClick={() => onClose()}
-					/>
-				</FloatButton.Group>
+        <FloatButton.Group shape="square" className="bottom-20 sm:bottom-4">
+          <FloatButton
+            icon={<Iconify icon={"material-symbols:filter-alt-outline"} />}
+          />
+          <FloatButton
+            icon={<Iconify icon={"material-symbols:add"} />}
+            onClick={() => onClose()}
+          />
+        </FloatButton.Group>
 
-				<CreateTailor
-					open={open}
-					onClose={onClose}
-				/>
-			</Container>
-		</>
-	);
+        <CreateTailor open={open} onClose={onClose} />
+      </Container>
+    </>
+  );
 };
 
 export default Tailor;
