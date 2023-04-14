36f619cbe954316e907cda6cd4ed5d655b55ccf0
Asatter833 <asatter833@gmail.com>
2023-04-14 17:13:10 +0600

    product backbutton removed

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,5 +1,4 @@
 import { useGetProducts } from "@/queries/products";
-import BackButton from "@components/BackButton";
 import DataTable from "@components/Datatable";
 import Iconify from "@components/iconify";
 import { Container, Grid, Typography } from "@mui/material";
@@ -11,88 +10,73 @@ import CreateProduct from "./components/CreateProduct";
 import ProductColumn from "./components/ProductColumn";
 
 const Products: React.FC = () => {
-	const { limit, setLimit, page, setPage, getQueryParams, setSearch, search } =
-		usePaginate();
+  const { limit, setLimit, page, setPage, getQueryParams, setSearch, search } =
+    usePaginate();
 
-	const { data, isLoading } = useGetProducts(getQueryParams());
-	console.log(data);
+  const { data, isLoading } = useGetProducts(getQueryParams());
+  console.log(data);
 
-	const { state: open, toggleState: onClose } = useToggle(false);
+  const { state: open, toggleState: onClose } = useToggle(false);
 
-	return (
-		<>
-			<Container
-				maxWidth={"lg"}
-				sx={{
-					maxWidth: "1500px !important",
-				}}
-			>
-				<Grid
-					container
-					rowGap={2}
-					direction="column"
-					marginTop={4}
-				>
-					<Grid className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-3">
-						<div className="flex flex-row items-center ">
-							<BackButton />
-							<Typography
-								variant="subtitle1"
-								fontWeight={700}
-							>
-								{/* {t("employee:EmployeeList")} */}
-								Products
-							</Typography>
-						</div>
-						<Input
-							className="w-full sm:max-w-xs"
-							placeholder="Search Products"
-							suffix={<BsSearch />}
-							value={search}
-							onChange={(e) => setSearch(e.target.value)}
-							style={{ width: "auto" }}
-							size="large"
-							allowClear
-						/>
-					</Grid>
+  return (
+    <>
+      <Container
+        maxWidth={"lg"}
+        sx={{
+          maxWidth: "1500px !important",
+        }}
+      >
+        <Grid container rowGap={2} direction="column" marginTop={4}>
+          <Grid className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-3">
+            <div className="flex flex-row items-center ">
+              <Typography variant="subtitle1" fontWeight={700}>
+                {/* {t("employee:EmployeeList")} */}
+                Products
+              </Typography>
+            </div>
+            <Input
+              className="w-full sm:max-w-xs"
+              placeholder="Search Products"
+              suffix={<BsSearch />}
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              style={{ width: "auto" }}
+              size="large"
+              allowClear
+            />
+          </Grid>
 
-					<Grid item>
-						<DataTable
-							columns={ProductColumn()}
-							rows={data?.data?.data || []}
-							isLoading={isLoading}
-							getRowId={(r: any) => r?._id}
-							// ss pagination
-							rowCount={data?.data?.total || 0}
-							paginationMode={"server"}
-							page={page}
-							onPageChange={setPage}
-							pageSize={limit}
-							onPageSizeChange={setLimit}
-						/>
-					</Grid>
-				</Grid>
+          <Grid item>
+            <DataTable
+              columns={ProductColumn()}
+              rows={data?.data?.data || []}
+              isLoading={isLoading}
+              getRowId={(r: any) => r?._id}
+              // ss pagination
+              rowCount={data?.data?.total || 0}
+              paginationMode={"server"}
+              page={page}
+              onPageChange={setPage}
+              pageSize={limit}
+              onPageSizeChange={setLimit}
+            />
+          </Grid>
+        </Grid>
 
-				<FloatButton.Group
-					shape="square"
-					className="bottom-20 sm:bottom-4"
-				>
-					<FloatButton
-						icon={<Iconify icon={"material-symbols:filter-alt-outline"} />}
-					/>
-					<FloatButton
-						icon={<Iconify icon={"material-symbols:add"} />}
-						onClick={() => onClose()}
-					/>
-				</FloatButton.Group>
+        <FloatButton.Group shape="square" className="bottom-20 sm:bottom-4">
+          <FloatButton
+            icon={<Iconify icon={"material-symbols:filter-alt-outline"} />}
+          />
+          <FloatButton
+            icon={<Iconify icon={"material-symbols:add"} />}
+            onClick={() => onClose()}
+          />
+        </FloatButton.Group>
 
-				<CreateProduct
-					open={open}
-					onClose={onClose}
-				/>
-			</Container>
-		</>
-	);
+        <CreateProduct open={open} onClose={onClose} />
+      </Container>
+    </>
+  );
 };
 
 export default Products;
