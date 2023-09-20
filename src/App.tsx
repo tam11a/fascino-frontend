2b28791b4f3990a0d083bd09e74e7f3e6afcbca3
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-20 21:22:34 +0600

    hot fix and cache buster

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,66 @@ import { StyleProvider } from "@ant-design/cssinjs";
 import { ConfigProvider } from "antd";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import CacheBuster from "react-cache-buster";
 
 const BaseRoutes = lazy(() => import("./routes"));
 
 const AuthProvider = lazy(() =>
-  import("@/contexts/AuthContext").then((module) => ({
-    default: module.AuthProvider,
-  }))
+	import("@/contexts/AuthContext").then((module) => ({
+		default: module.AuthProvider,
+	}))
 );
 
 const query = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
 });
 
+import { version } from "../package.json";
+
 const App: React.FC = () => {
-  return (
-    <QueryClientProvider client={query}>
-      <ThemeProvider theme={theme}>
-        <StyleProvider hashPriority="high">
-          <ConfigProvider
-            theme={{
-              token: {
-                colorPrimary: theme.palette.primary.main,
-                borderRadius: 4,
-                fontFamily: theme.typography.fontFamily,
-              },
-            }}
-          >
-            {/* <CssBaseline /> */}
-            <BrowserRouter>
-              <AuthProvider>
-                <BaseRoutes />
-              </AuthProvider>
-            </BrowserRouter>
-          </ConfigProvider>
-        </StyleProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
-  );
+	const isProduction = import.meta.env.VITE_NODE_ENV === "production";
+	return (
+		<CacheBuster
+			currentVersion={version}
+			isEnabled={isProduction} //If false, the library is disabled.
+			isVerboseMode={false} //If true, the library writes verbose logs to console.
+			loadingComponent={
+				<>
+					<div className="h-full w-full flex flex-row items-center justify-center">
+						<p className="font-bold">Updating to New Version...</p>
+					</div>
+				</>
+			} //If not pass, nothing appears at the time of new version check.
+			metaFileDirectory={"."} //If public assets are hosted somewhere other than root on your server.
+		>
+			<QueryClientProvider client={query}>
+				<ThemeProvider theme={theme}>
+					<StyleProvider hashPriority="high">
+						<ConfigProvider
+							theme={{
+								token: {
+									colorPrimary: theme.palette.primary.main,
+									borderRadius: 4,
+									fontFamily: theme.typography.fontFamily,
+								},
+							}}
+						>
+							{/* <CssBaseline /> */}
+							<BrowserRouter>
+								<AuthProvider>
+									<BaseRoutes />
+								</AuthProvider>
+							</BrowserRouter>
+						</ConfigProvider>
+					</StyleProvider>
+				</ThemeProvider>
+			</QueryClientProvider>
+		</CacheBuster>
+	);
 };
 
 export default App;
