4c68a315b22aa38a255dd661c55d68bf622c8bc3
tam11a <ibrahimsadiktamim@gmail.com>
2023-11-14 15:34:22 +0600

    done download button

--- src/queries/order/index.ts
+++ src/queries/order/index.ts
@@ -2,77 +2,93 @@ import instance from "@/services";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const getOrders = (params: any) => {
-  console.log(params);
-  return instance.get(`/order`, {
-    params,
-  });
+	return instance.get(`/order`, {
+		params,
+	});
 };
 
 export const useGetOrders = (params: any) => {
-  return useQuery(
-    [
-      "get-all-order",
-      params,
-      {
-        params,
-      },
-    ],
-    () => getOrders(params),
-    {
-      // enabled: !!vendorId,
-      // select: (data: string) => data?.data || [],
-    }
-  );
+	return useQuery(
+		[
+			"get-all-order",
+			params,
+			{
+				params,
+			},
+		],
+		() => getOrders(params),
+		{
+			// enabled: !!vendorId,
+			// select: (data: string) => data?.data || [],
+		}
+	);
+};
+
+export const downloadOrders = (params: any) => {
+	return instance.get(`/order/download`, {
+		params,
+		headers: {
+			responseType: "blob",
+		},
+	});
+};
+
+export const useDownloadOrders = () => {
+	return useMutation(downloadOrders, {
+		onSuccess: (data: any) => {
+			return new Blob(data);
+		},
+	});
 };
 
 const getOrderById = (id: any) => {
-  return instance.get(`/order/${id}`, {
-    // params: {},
-  });
+	return instance.get(`/order/${id}`, {
+		// params: {},
+	});
 };
 
 export const useGetOrderById = (id: any) => {
-  return useQuery(["get-order-by-id", id], () => getOrderById(id), {
-    enabled: !!id,
-    // select: (data: string) => data?.data || [],
-  });
+	return useQuery(["get-order-by-id", id], () => getOrderById(id), {
+		enabled: !!id,
+		// select: (data: string) => data?.data || [],
+	});
 };
 
 const getItemsByOrderId = (id: any) => {
-  return instance.get(`/order/orderlines/${id}`, {
-    // params: {},
-  });
+	return instance.get(`/order/orderlines/${id}`, {
+		// params: {},
+	});
 };
 
 export const useGetItemsByOrderId = (id: any) => {
-  return useQuery(["get-items-by-order-id", id], () => getItemsByOrderId(id), {
-    enabled: !!id,
-    // select: (data: string) => data?.data || [],
-  });
+	return useQuery(["get-items-by-order-id", id], () => getItemsByOrderId(id), {
+		enabled: !!id,
+		// select: (data: string) => data?.data || [],
+	});
 };
 
 const postOrder = (data: any) => {
-  return instance.post(`/order`, data);
+	return instance.post(`/order`, data);
 };
 
 export const usePostOrder = () => {
-  const queryClient = useQueryClient();
-  return useMutation(postOrder, {
-    onSuccess: () => {
-      queryClient.invalidateQueries(["get-orders"]);
-    },
-  });
+	const queryClient = useQueryClient();
+	return useMutation(postOrder, {
+		onSuccess: () => {
+			queryClient.invalidateQueries(["get-orders"]);
+		},
+	});
 };
 
 const addTransaction = ({ data, id }: { data: any; id: any }) => {
-  return instance.patch(`/order/${id}/transaction`, data);
+	return instance.patch(`/order/${id}/transaction`, data);
 };
 
 export const useAddTransaction = () => {
-  const queryClient = useQueryClient();
-  return useMutation(addTransaction, {
-    onSuccess: () => {
-      queryClient.invalidateQueries(["get-order-by-id"]);
-    },
-  });
+	const queryClient = useQueryClient();
+	return useMutation(addTransaction, {
+		onSuccess: () => {
+			queryClient.invalidateQueries(["get-order-by-id"]);
+		},
+	});
 };
